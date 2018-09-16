d594834af8b6fbfe12bbf86f6621485fedf99af3
DimaGashko <dgashko@mail.ua>
2018-09-16 23:42:51 +0300

    edit

--- IDE project/Pacman/Pacman/Main.cpp
+++ IDE project/Pacman/Pacman/Main.cpp
@@ -3,8 +3,19 @@
 using namespace std;
 
 int main() {
-	cout << "Hi" << endl;
+	int n = 0;
+	while (1) {
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 10; j++) {
+				cout << n % 10;
+			}
 
+			cout << endl;
+		}
+
+		system("cls");
+		n++;
+	}
 	system("pause");
 
 	return 0;
